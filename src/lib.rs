0873dc5b220b32b8d15a935f71be1a767b80d397
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-01-10 19:41:57 -0700

    Minor documentation change

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 //! [Newtype](https://doc.rust-lang.org/rust-by-example/generics/new_types.html) offering some utility methods
 //! for [zero-based indices](https://en.wikipedia.org/wiki/Zero-based_numbering)
 //!
-//! In order to keep the lengths of methods reasonable, several abbreviations have been used, namely
+//! In order to keep the lengths of method names reasonable, several abbreviations have been used, namely
 //! * [**len**](https://www.abbreviations.com/term/92110) for length;
 //! * [**intvl**](https://www.abbreviations.com/term/563139) for interval;
 //! * [**int**](https://www.abbreviations.com/term/542972) for integer.
