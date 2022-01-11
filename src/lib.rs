5224764b9313b8f26bc79cb7bab17a246580b78d
Dmitrii - Demenev <demenev.dmitriy1@mail.ru>
2022-01-10 20:58:47 -0700

    Added a separate README.md

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,4 @@
-//! [Newtype](https://doc.rust-lang.org/rust-by-example/generics/new_types.html) offering some utility methods
-//! for [zero-based indices](https://en.wikipedia.org/wiki/Zero-based_numbering)
-//!
-//! In order to keep the lengths of method names reasonable, several abbreviations have been used, namely
-//! * [**len**](https://www.abbreviations.com/term/92110) for length;
-//! * [**intvl**](https://www.abbreviations.com/term/563139) for interval;
-//! * [**int**](https://www.abbreviations.com/term/542972) for integer.
-//!
-//! # Examples
-//!
-//! ## Base case
-//! ```
-//! use zero_based_index::ZeroBasedIndex;
-//!
-//! let zbi = ZeroBasedIndex::<usize>(2);
-//! assert_eq!(zbi.try_get_len_of_closed_int_intvl_from_0(), Some(3));
-//! ```
-//! 
-//! ## Base case with `zero_based_index::AsZeroBasedIndex`
-//! ```
-//! use zero_based_index::{ZeroBasedIndex, AsZeroBasedIndex};
-//! 
-//! let zbi = 2.as_zero_based_index();
-//! assert_eq!(zbi.try_get_len_of_closed_int_intvl_from_0(), Some(3));
-//! ```
-//!
-//! ## Corner case
-//! ```
-//! use zero_based_index::ZeroBasedIndex;
-//!
-//! let zbi = ZeroBasedIndex::<usize>(usize::MAX);
-//! assert_eq!(zbi.try_get_len_of_closed_int_intvl_from_0(), None);
-//! ```
+#![doc = include_str!("../README.md")]
 #![cfg_attr(not(any(doc, test, doctest, feature = "std")), no_std)]
 #![cfg_attr(any(doc, test, doctest, feature = "unchecked_math"), feature(unchecked_math))]
 
